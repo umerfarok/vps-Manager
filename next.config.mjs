82ddfadee1f1d77fab2215a0dd2b4dbc339efb3b
umerfarok <umerfarooq.dev@gmail.com>
2024-07-29 20:57:16 +0500

    Initial commit from Create Next App

---
+++ next.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        net: false,
+        tls: false,
+        fs: false,
+      };
+    }
+    return config;
+  },
+};
+
+module.exports = nextConfig;
